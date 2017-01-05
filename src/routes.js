fc3ca1195169fc2aebe88dac82eb568d81e497d1
Chris Olsen <olsen.chris@gmail.com>
2017-01-05 10:35:43 -0700

    include /signin in routes

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,7 @@
 import Hello from './components/Hello'
+import Signin from './components/Signin'
 
 export default [
     { path: '/', name: 'root', component: Hello },
+    { path: '/signin', name: 'root', component: Signin },
 ]
