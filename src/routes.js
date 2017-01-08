7827dba0fee77e59768485a5fe1216efaff34ff7
Chris Olsen <olsen.chris@gmail.com>
2017-01-08 11:37:30 -0700

    fix route spacing

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,6 @@ import SignIn from './components/SignIn'
 import Hello from './components/Hello'
 
 export default [
-    { path: '/signin', name: 'signIn', component: SignIn },
-    { path: '/hello', name: 'hello', component: Hello },
+  { path: '/signin', name: 'signIn', component: SignIn },
+  { path: '/hello', name: 'hello', component: Hello },
 ]
