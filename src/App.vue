1fc3d16ca3b0522789d4b6c3cfe713f42d85fe8f
Chris Olsen <olsen.chris@gmail.com>
2017-01-08 10:40:46 -0700

    use purecss for base css

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,139 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <hello></hello>
+  <div id="layout">
+    <a href="#menu" id="menuLink" class="menu-link">
+      <span></span>
+    </a>
+
+    <div id="menu">
+      <div class="pure-menu">
+        <a class="pure-menu-heading" href="#">Company</a>
+        <ul class="pure-menu-list">
+          <li class="pure-menu-item">
+            <router-link :to="{name: 'signIn'}" class="pure-menu-link">Home</router-link>
+          </li>
+          <li class="pure-menu-item">
+            <router-link :to="{name: 'hello'}" class="pure-menu-link">Hello</router-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+
+    <div id="main">
+      <div class="content">
+        <Message v-bind:type="flash.type" v-bind:message="flash.message" v-bind:duration="flash.duration" />
+        <transition name="fade">
+          <router-view></router-view>
+        </transition>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello';
 import config from '../config'
+import Message from './components/common/Message'
 
 const env = config.getEnv()
 
 export default {
   name: 'app',
   components: {
-    Hello,
+    Message,
   },
-  mounted() {
-    const fbid = env.facebookAppID
-    if (!fbid) {
-      window.console.error("need to enter facebook app id here")
-      return
-    }
+  computed: {
+    currentUser() {
+      return this.$store.state.currentUser || {}
+    },
 
-    window.fbAsyncInit = function fninit() {
-      window.FB.init({
-        appId: fbid,
-        cookie: true,
-        xfbml: true,
-        version: 'v2.8',
-      })
-    }
+    flash() {
+      return this.$store.state.flash || {}
+    },
+  },
 
-    const setup = function fnsetup(d, s, id) {
-      if (d.getElementById(id)) {
+  methods: {
+    bindFacebook() {
+      const fbid = env.facebookAppID
+      if (!fbid) {
+        window.console.error("need to enter facebook app id here")
         return
       }
-      const fjs = d.getElementsByTagName(s)[0]
-      const js = d.createElement(s)
-      js.id = id
-      js.src = "//connect.facebook.net/en_US/sdk.js"
-      fjs.parentNode.insertBefore(js, fjs)
-    }
 
-    setup(window.document, 'script', 'facebook-jssdk')
+      window.fbAsyncInit = function fninit() {
+        window.FB.init({
+          appId: fbid,
+          cookie: true,
+          xfbml: true,
+          version: 'v2.8',
+        })
+      }
+
+      const setup = function fnsetup(d, s, id) {
+        if (d.getElementById(id)) {
+          return
+        }
+        const fjs = d.getElementsByTagName(s)[0]
+        const js = d.createElement(s)
+        js.id = id
+        js.src = "//connect.facebook.net/en_US/sdk.js"
+        fjs.parentNode.insertBefore(js, fjs)
+      }
+
+      setup(window.document, 'script', 'facebook-jssdk')
+    },
+
+    bindSidebar() {
+      const d = window.document
+      const layout = d.getElementById('layout')
+      const menu = d.getElementById('menu')
+      const menuLink = d.getElementById('menuLink')
+      const content = d.getElementById('main')
+
+      const toggleClass = (element, className) => {
+        const classes = element.className.split(/\s+/)
+        const initLength = classes.length
+
+        classes.forEach((cls, i) => {
+          if (cls === className) {
+            classes.splice(i, 1)
+          }
+        })
+
+        // The className is not found
+        if (initLength === classes.length) {
+          classes.push(className);
+        }
+
+        element.className = classes.join(' ');
+      }
+
+      const toggleAll = e => {
+        const active = 'active'
+        e.preventDefault();
+        toggleClass(layout, active)
+        toggleClass(menu, active)
+        toggleClass(menuLink, active)
+      }
+
+      menuLink.onclick = e => {
+        toggleAll(e)
+      }
+
+      content.onclick = e => {
+        if (menu.className.indexOf('active') !== -1) {
+          toggleAll(e)
+        }
+      }
+    },
+  },
+
+  mounted() {
+    this.bindFacebook()
+    this.bindSidebar()
   },
 };
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import './scss/pure.css';
+@import './scss/sidebar.css';
 </style>
