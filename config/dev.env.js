df9765f0ca5a33cd206b6026b8af097507034c59
Chris Olsen <olsen.chris@gmail.com>
2017-01-27 11:34:44 -0700

    updates

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,7 +4,7 @@ var prodEnv = require('./prod.env')
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   port: 3000,
-  uri: 'http://dev.myapp.io',
-  facebookAppID: '',
+  uri: 'http://dev.leaguesmart.com',
+  facebookAppID: '363028790729481',
   apiUrl: 'http://localhost:8888',
 })
